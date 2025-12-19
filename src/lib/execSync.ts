a9c8d0dac1aff2169546a55c6cab7655759ab3fb
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-18 21:11:41 -0800

    Use node-version-call-local bindSync

--- src/lib/execSync.ts
+++ src/lib/execSync.ts
@@ -1,21 +1,19 @@
 import cp from 'child_process';
-import Module from 'module';
+import { bindSync } from 'node-version-call-local';
 import path from 'path';
 import url from 'url';
 
-const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
+const workerPath = path.join(__dirname, '..', '..', 'cjs', 'lib', 'execSync.js');
 
-// Worker MUST always load from dist/cjs/ for old Node compatibility
-const workerPath = path.join(__dirname, '..', '..', 'cjs', 'lib', 'worker.js');
+function run(cmd: string, options: { encoding?: BufferEncoding } = {}): string {
+  return cp.execSync(cmd, options) as unknown as string;
+}
 
-let functionExec = null; // break dependencies
+// execSync was added in Node 0.12
+// spawnOptions: false - no node/npm spawn (shell commands only)
+const worker = typeof cp.execSync === 'function' ? run : bindSync('>=0.12', workerPath, { spawnOptions: false });
+
 export default function execSync(cmd: string, options: { encoding?: BufferEncoding } = {}): string {
-  // Use native execSync if available (Node 0.12+)
-  if (typeof cp.execSync === 'function') {
-    return cp.execSync(cmd, options) as unknown as string;
-  }
-
-  if (!functionExec) functionExec = _require('function-exec-sync');
-  return functionExec?.({ callbacks: true }, workerPath, cmd, options);
+  return worker(cmd, options) as string;
 }
