dd27af22e13cf40909823985a621696a7294703f
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-23 09:30:15 -0800

    Upgrade modules

--- src/lib/execSync.ts
+++ src/lib/execSync.ts
@@ -10,9 +10,11 @@ function run(cmd: string, options: { encoding?: BufferEncoding } = {}): string {
   return cp.execSync(cmd, options) as unknown as string;
 }
 
+type execSyncFunction = (cmd: string, options?: { encoding?: BufferEncoding }) => string;
+
 // execSync was added in Node 0.12
 // spawnOptions: false - no node/npm spawn (shell commands only)
-const worker = typeof cp.execSync === 'function' ? run : bindSync('>=0.12', workerPath, { spawnOptions: false });
+const worker = (typeof cp.execSync === 'function' ? run : bindSync('>=0.12', workerPath, { spawnOptions: false })) as execSyncFunction;
 
 export default function execSync(cmd: string, options: { encoding?: BufferEncoding } = {}): string {
   return worker(cmd, options) as string;
